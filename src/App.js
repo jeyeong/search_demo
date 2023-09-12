906da15065a6b5c11b97e438bb65f06d8ad2a09c
Je Yeong Soh <sohjeyeong@gmail.com>
2023-09-12 00:22:42 -0700

    implemented basic structure

--- src/App.js
+++ src/App.js
@@ -1,90 +1,115 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
+import { Chip, createTheme, ThemeProvider } from '@mui/material'
+import debounce from 'lodash.debounce'
 
 import SearchIcon from '@mui/icons-material/Search'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#575be1',
+      contrastText: '#fff',
+    },
+  },
+})
+
+const CATALOGS = [
+  { name: 'Nike', id: 'nike' },
+  { name: 'Walmart', id: 'walmart' },
+  { name: 'Netflix', id: 'netflix' },
+]
+
 const App = () => {
-  const searchQueryRef = useRef()
-  const datasetSelectRef = useRef()
   const [loading, setLoading] = useState(false)
+  const [catalogSelected, setCatalogSelected] = useState('nike')
   const [searchResults, setSearchResults] = useState([])
 
-  const submitHandler = (e) => {
-    e.preventDefault() // prevent page refresh
-
-    setLoading(true)
-
-    fetch(
-      `https://l2clhcofuc.execute-api.us-east-1.amazonaws.com/prod/pinecone?query=${searchQueryRef.current.value}&catalog_id=${datasetSelectRef.current.value}`
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        setSearchResults(res.matches)
-        setLoading(false)
-      })
-  }
+  const [debouncedSearchHandler] = useState(() => {
+    const searchHandler = (query, catalogId) => {
+      fetch(
+        `https://l2clhcofuc.execute-api.us-east-1.amazonaws.com/prod/pinecone?query=${query}&catalog_id=${catalogId}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          setSearchResults(res.matches)
+          setLoading(false)
+        })
+    }
+    return debounce(searchHandler, 1000)
+  })
 
   return (
-    <div className="App">
-      <form
-        style={{
-          // backgroundColor: '#8a62aa',
-          padding: 10,
-          border: '1.5px solid rgb(221, 221, 221)',
-          borderRadius: 50,
-          boxShadow: '0 1px 2px rgba(0,0,0,0.08), 0 4px 12px rgba(0,0,0,0.05)',
-          width: 'max-content',
-        }}
-      >
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div
-            style={{
-              paddingLeft: '4px',
-              paddingRight: '8px',
-              display: 'grid',
-              placeItems: 'center',
-            }}
-          >
-            <SearchIcon fontSize="small" />
-          </div>
-          <input
-            ref={searchQueryRef}
-            name="query-box"
-            style={{
-              border: 'none',
-              outline: 'none',
-              width: '300px',
-              fontSize: '16px',
-            }}
-            type="search"
-            autoComplete="off"
-            autoCorrect="off"
-            autoCapitalize="off"
-            spellCheck="false"
-            placeholder="Search anything"
-          />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <div style={{ display: 'flex', columnGap: 6 }}>
+          {CATALOGS.map(({ name, id }) => (
+            <Chip
+              label={name}
+              onClick={() => {
+                setCatalogSelected(id)
+              }}
+              color={'primary'}
+              variant={id === catalogSelected ? 'filled' : 'outlined'}
+            />
+          ))}
         </div>
-      </form>
 
-      <div>
-        <p style={{ marginBottom: 0 }}>Dataset</p>
-        <select ref={datasetSelectRef}>
-          <option value="nike">Nike</option>
-          <option value="walmart">Walmart</option>
-        </select>
-      </div>
+        <form
+          style={{
+            marginTop: 12,
+            padding: 10,
+            border: '1.5px solid rgb(221, 221, 221)',
+            borderRadius: 50,
+            boxShadow:
+              '0 1px 2px rgba(0,0,0,0.08), 0 4px 12px rgba(0,0,0,0.05)',
+            width: 'max-content',
+          }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div
+              style={{
+                paddingLeft: '4px',
+                paddingRight: '8px',
+                display: 'grid',
+                placeItems: 'center',
+              }}
+            >
+              <SearchIcon fontSize="small" />
+            </div>
+            <input
+              onChange={(e) => {
+                setLoading(true)
+                debouncedSearchHandler(e.target.value, catalogSelected)
+              }}
+              name="query-box"
+              style={{
+                border: 'none',
+                outline: 'none',
+                width: '350px',
+                fontSize: '16px',
+              }}
+              type="search"
+              autoComplete="off"
+              autoCorrect="off"
+              autoCapitalize="off"
+              spellCheck="false"
+              placeholder="Search anything"
+            />
+          </div>
+        </form>
 
-      <div>
-        <h2>Search Results</h2>
-        {loading
-          ? 'loading...'
-          : searchResults.map(({ metadata }) => (
-              <div key={metadata.name}>
-                <h4>{metadata.name}</h4>
-                <p>{metadata.description}</p>
-              </div>
-            ))}
+        <div>
+          {loading
+            ? 'loading...'
+            : searchResults.map(({ metadata }) => (
+                <div key={metadata.name}>
+                  <h4>{metadata.name}</h4>
+                  <p>{metadata.description}</p>
+                </div>
+              ))}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
